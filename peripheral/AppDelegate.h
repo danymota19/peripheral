b3eb5732070359df32f791ec2b66cbcb825afcc7
Daniela Mota <daniela@Agustins-MBP.lan>
2015-05-31 18:30:48 -0500

    Initial Commit

---
+++ peripheral/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  peripheral
+//
+//  Created by Daniela Mota on 5/31/15.
+//  Copyright (c) 2015 Daniela Mota. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ peripheral/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  peripheral
+//
+//  Created by Daniela Mota on 5/31/15.
+//  Copyright (c) 2015 Daniela Mota. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
